f0df001bf0786cb3172c84cf763ca95492d08a76
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-18 18:11:08 +0530

    Update Timelock.test.js

--- test/Timelock.test.js
+++ test/Timelock.test.js
@@ -1,8 +1,8 @@
 const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const ethers = require('ethers');
-const CakeToken = artifacts.require('CakeToken');
+const PieToken = artifacts.require('PieToken');
 const MasterChef = artifacts.require('MasterChef');
-const MockBEP20 = artifacts.require('libs/MockBEP20');
+const MockOIP20 = artifacts.require('libs/MockOIP20');
 const Timelock = artifacts.require('Timelock');
 const SyrupBar = artifacts.require('SyrupBar');
 
@@ -13,23 +13,23 @@ function encodeParameters(types, values) {
 
 contract('Timelock', ([alice, bob, carol, dev, minter]) => {
     beforeEach(async () => {
-        this.cake = await CakeToken.new({ from: alice });
+        this.pie = await PieToken.new({ from: alice });
         this.timelock = await Timelock.new(bob, '28800', { from: alice }); //8hours
     });
 
     it('should not allow non-owner to do operation', async () => {
-        await this.cake.transferOwnership(this.timelock.address, { from: alice });
+        await this.pie.transferOwnership(this.timelock.address, { from: alice });
         await expectRevert(
-            this.cake.transferOwnership(carol, { from: alice }),
+            this.pie.transferOwnership(carol, { from: alice }),
             'Ownable: caller is not the owner',
         );
         await expectRevert(
-            this.cake.transferOwnership(carol, { from: bob }),
+            this.pie.transferOwnership(carol, { from: bob }),
             'Ownable: caller is not the owner',
         );
         await expectRevert(
             this.timelock.queueTransaction(
-                this.cake.address, '0', 'transferOwnership(address)',
+                this.pie.address, '0', 'transferOwnership(address)',
                 encodeParameters(['address'], [carol]),
                 (await time.latest()).add(time.duration.hours(6)),
                 { from: alice },
@@ -39,34 +39,34 @@ contract('Timelock', ([alice, bob, carol, dev, minter]) => {
     });
 
     it('should do the timelock thing', async () => {
-        await this.cake.transferOwnership(this.timelock.address, { from: alice });
+        await this.pie.transferOwnership(this.timelock.address, { from: alice });
         const eta = (await time.latest()).add(time.duration.hours(9));
         await this.timelock.queueTransaction(
-            this.cake.address, '0', 'transferOwnership(address)',
+            this.pie.address, '0', 'transferOwnership(address)',
             encodeParameters(['address'], [carol]), eta, { from: bob },
         );
         await time.increase(time.duration.hours(1));
         await expectRevert(
             this.timelock.executeTransaction(
-                this.cake.address, '0', 'transferOwnership(address)',
+                this.pie.address, '0', 'transferOwnership(address)',
                 encodeParameters(['address'], [carol]), eta, { from: bob },
             ),
             "Timelock::executeTransaction: Transaction hasn't surpassed time lock.",
         );
         await time.increase(time.duration.hours(8));
         await this.timelock.executeTransaction(
-            this.cake.address, '0', 'transferOwnership(address)',
+            this.pie.address, '0', 'transferOwnership(address)',
             encodeParameters(['address'], [carol]), eta, { from: bob },
         );
-        assert.equal((await this.cake.owner()).valueOf(), carol);
+        assert.equal((await this.pie.owner()).valueOf(), carol);
     });
 
     it('should also work with MasterChef', async () => {
-        this.lp1 = await MockBEP20.new('LPToken', 'LP', '10000000000', { from: minter });
-        this.lp2 = await MockBEP20.new('LPToken', 'LP', '10000000000', { from: minter });
+        this.lp1 = await MockOIP20.new('LPToken', 'LP', '10000000000', { from: minter });
+        this.lp2 = await MockOIP20.new('LPToken', 'LP', '10000000000', { from: minter });
         this.syrup = await SyrupBar.new(this.cake.address, { from: minter });
-        this.chef = await MasterChef.new(this.cake.address, this.syrup.address, dev, '1000', '0', { from: alice });
-        await this.cake.transferOwnership(this.chef.address, { from: alice });
+        this.chef = await MasterChef.new(this.pie.address, this.syrup.address, dev, '1000', '0', { from: alice });
+        await this.pie.transferOwnership(this.chef.address, { from: alice });
         await this.syrup.transferOwnership(this.chef.address, { from: minter });
         await this.chef.add('100', this.lp1.address, true, { from: alice });
         await this.chef.transferOwnership(this.timelock.address, { from: alice });
