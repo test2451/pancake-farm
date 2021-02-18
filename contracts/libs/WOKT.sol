856f81b1877c5fd9619a65c4405bd1a06f2e20dc
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-18 19:20:06 +0530

    Update and rename WBNB.sol to WOKT.sol

--- contracts/libs/WOKT.sol
+++ contracts/libs/WOKT.sol
@@ -1,8 +1,8 @@
 pragma solidity >0.4.18;
 
-contract WBNB {
-    string public name     = "Wrapped BNB";
-    string public symbol   = "WBNB";
+contract WOKT {
+    string public name     = "Wrapped OKT";
+    string public symbol   = "WOKT";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
